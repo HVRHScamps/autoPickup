bf8dfcb5d7640f7dda3072605bdab2e3ae37172f
Addie <adiorio@nceschool.org>
2022-06-22 11:28:34 -0400

    Controller and pickup

--- autoPickup.py
+++ autoPickup.py
@@ -1,19 +1,28 @@
+from tracemalloc import stop
 import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
-def pickup(self):
-    self.robot.pickupMotor.Set(1)
-    self.robot.pickupPneumatic.Extend()
-    self.robot.beltZ1.Set(-0.8)
-    self.robot.beltZ2.Set(0)
-    self.robot.beltZ3.Set(0)
-    self.robot.upperTension.Retract()
-    self.robot.lowerTension.Extend()
+def pickup(robot):
+    robot.pickupMotor.Set(1)
+    robot.pickupPneumatic.Extend()
+    robot.beltZ1.Set(-0.8)
+    robot.beltZ2.Set(0)
+    robot.beltZ3.Set(0)
+    robot.upperTension.Retract()
+    robot.lowerTension.Extend()
 
 def main(robot: libhousy.robot):
     #Here is where your recurring code will go
     if robot.controller.getAxis(robot.controller.Axis.lTrigger) > 0.8:
         pickup()
-        return libhousy.DONE
+
+    else:
+        robot.pickupMotor.Set(0)
+        robot.pickupPneumatic.Retract()
+        robot.beltZ1.Set(0)
+        robot.beltZ2.Set(0)
+        robot.beltZ3.Set(0)
+    
+        
 
     # This tells the robot we're done and it can move on
        

3b25e0b48f435fc0993490a454daed46eba13d93
cannotilever <51347140+cannotilever@users.noreply.github.com>
2022-06-26 11:14:23 -0400

    reset for week 2

--- autoPickup.py
+++ autoPickup.py
@@ -1,29 +1,9 @@
-from tracemalloc import stop
+# inclue code to help us talk to the robot
 import libhousy
-#You can define helper functions here, make sure to but them *above* the main function
-def pickup(robot):
-    print("running pickup mode")
-    robot.pickupMotor.Set(1)
-    robot.pickupPneumatic.Extend()
-    robot.beltZ1.Set(-0.8)
-    robot.beltZ2.Set(0)
-    robot.beltZ3.Set(0)
-    robot.upperTension.Retract()
-    robot.lowerTension.Extend()
 
 def main(robot: libhousy.robot):
-    #Here is where your recurring code will go
-    if robot.controller.getAxis(robot.controller.Axis.lTrigger) > 0.8:
-        pickup(robot)
-
-    else:
-        robot.pickupMotor.Set(0)
-        robot.pickupPneumatic.Retract()
-        robot.beltZ1.Set(0)
-        robot.beltZ2.Set(0)
-        robot.beltZ3.Set(0)
+    # Here is where your recurring code will go
+    print("Hello World!")
     
-        
-
-    # This tells the robot we're done and it can move on
-       
+    # After everything is done, we tell the main program to stop us
+    return libhousy.DONE

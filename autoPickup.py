e9941dc861fc4636536224b208547fae4013b776
Logan <loganpadelli@gmail.com>
2022-06-29 11:14:37 -0400

    Readded improtant machanics

--- autoPickup.py
+++ autoPickup.py
@@ -1,9 +1,27 @@
 # inclue code to help us talk to the robot
+from tracemalloc import stop
 import libhousy
+
+def pickup(robot):
+    print("running pickup mode")
+    robot.pickupMotor.set(1)
+    robot.pickupPneumatic.Extend()
+    robot.beltZ1(-0.8)
+    robot.beltZ2(0)
+    robot.beltZ3(0)
+    robot.upperTension.retract()
+    robot.lowerTension.retract()
 
 def main(robot: libhousy.robot):
     # Here is where your recurring code will go
-    print("Hello World!")
-    
+    if robot.controller.getAxis(robot.controller.Axis.lTrigger) > 0.8:
+        pickup(robot)
+   
+    else:
+        robot.pickupMotor.set(0)
+        robot.pickupPneumatic.Retract()
+        robot.beltZ1(0)
+        robot.beltZ2(0)
+        robot.beltZ3(0)
+
     # After everything is done, we tell the main program to stop us
-    return libhousy.DONE
